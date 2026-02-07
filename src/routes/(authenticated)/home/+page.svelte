c19a20b650d7e49c8b42bafacb68918011c469fb
Julian One <38872371+julian-one@users.noreply.github.com>
2026-02-07 00:59:19 -0700

    home is behind, the world ahead

--- src/routes/(authenticated)/home/+page.svelte
+++ src/routes/(authenticated)/home/+page.svelte
@@ -1,6 +1,21 @@
-<div class="flex min-h-screen flex-col items-center justify-center p-4">
-	<div class="text-center">
-		<h1 class="text-4xl font-bold">Home Page</h1>
-		<p class="mt-2 text-lg">Welcome home</p>
+<script lang="ts">
+	import { afterNavigate } from '$app/navigation';
+
+	afterNavigate(() => {
+		window.scrollTo(0, document.body.scrollHeight);
+	});
+</script>
+
+<div
+	class="hero min-h-screen"
+	style="background-image: url('/images/shire.png')"
+>
+	<div class="hero-overlay"></div>
+	<div class="hero-content text-neutral-content flex h-full w-full flex-col items-start justify-end text-start">
+		<div class="max-w-full p-4 pb-8 md:max-w-2xl lg:max-w-3xl">
+			<h1 class="mb-5 text-xl leading-tight font-bold md:text-2xl lg:text-3xl">
+				<i>If more of us valued food and cheer and song above hoarded gold, it would be a merrier world.</i>
+			</h1>
+		</div>
 	</div>
 </div>
