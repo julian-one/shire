d1725eedcbaac4af25b2aaafc51bd75d88b8fdc3
Julian One <38872371+julian-one@users.noreply.github.com>
2026-03-14 10:17:04 -0600

    no logs

--- src/lib/controllers/log.ts
+++
@@ -1,9 +0,0 @@
-import { Citadel } from '$lib/controllers/citadel';
-import type { LogResponse } from '$lib/types/log';
-
-export class LogController {
-	async Fetch(source?: string, lines?: number): Promise<LogResponse> {
-		const response = await Citadel.get('/logs', { params: { source, lines } });
-		return response.data as LogResponse;
-	}
-}
